fc27c26947bfad8187707c5f7067e32f7c9c66c3
FatimaLDL1997 <fatima.labade@ontariotechu.net>
2022-01-31 11:17:19 -0500

    jsFixed

---
+++ js/script2.js
@@ -0,0 +1,50 @@
+
+$(document).ready(function(){
+    console.log('hi'); 
+    const date = document.getElementById('date'); 
+    // date.innerHTML = new Date().getFullYear(); 
+
+    const btnHamburger = document.querySelector('#btnHamburger'); 
+    const body = document.querySelector('body'); 
+    const header = document.querySelector('.header'); 
+    const overlay = document.querySelector('.overlay'); 
+    const fadeElems = document.querySelectorAll('.has-fade'); 
+    const links = document.querySelector('.header__menu');
+    const topLink = document.querySelector(".top-link"); 
+    
+    //menu bar open and fading 
+    [btnHamburger, links].forEach(item => {
+        item.addEventListener('click', event => {
+          
+          body.classList.toggle('noscroll');
+
+        if(header.classList.contains('open')){//close ham menu
+            header.classList.remove('open'); 
+            fadeElems.forEach(function(element){
+                element.classList.remove('fade-in'); 
+                element.classList.add('fade-out'); 
+            });
+        }
+        else{//open ham menu
+            header.classList.add('open');
+            fadeElems.forEach(function(element){
+                element.classList.remove('fade-out');  
+                element.classList.add('fade-in'); 
+            }); 
+        }
+        }); 
+      }); 
+   
+});
+
+    //preloader
+    const preloader = document.querySelector(".preloader"); 
+
+    window.addEventListener("load", function() {
+        preloader.classList.add("hide-preloader");
+    }); 
+
+
+
+
+
