2d91f60915804d2ce96dc39e3b68563e6dc55f3c
FatimaLDL1997 <fatima.labade@ontariotechu.net>
2022-04-23 02:55:22 -0400

    pause btn Fixed

--- js/script2.js
+++ js/script2.js
@@ -44,6 +44,19 @@ $(document).ready(function(){
         preloader.classList.add("hide-preloader");
     }); 
 
+    
+
+    btn.addEventListener("click", function () {
+        if (!btn.classList.contains("slide")) {
+            btn.classList.add("slide");
+            video.pause();
+        } else {
+            btn.classList.remove("slide");
+            video.play();
+        }
+    });
+
+
 
 
 
