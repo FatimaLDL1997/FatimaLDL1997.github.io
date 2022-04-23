63ccda3ac01e0d6be6e4c8be3c04112e13d4f425
FatimaLDL1997 <fatima.labade@ontariotechu.net>
2022-04-23 02:58:10 -0400

    btnFix

--- js/script2.js
+++ js/script2.js
@@ -46,6 +46,11 @@ $(document).ready(function(){
 
     
 
+    //video switch
+    const btn = document.querySelector(".switch-btn");
+    const video = document.querySelector(".video-container");
+
+    
     btn.addEventListener("click", function () {
         if (!btn.classList.contains("slide")) {
             btn.classList.add("slide");
