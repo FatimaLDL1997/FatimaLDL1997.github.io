70ce5d728cfa9d6e220f5629f66892adaed701c1
FatimaLDL1997 <fatima.labade@ontariotechu.net>
2022-05-11 22:07:03 -0400

    fixed Footer

--- js/script2.js
+++ js/script2.js
@@ -46,20 +46,20 @@ $(document).ready(function(){
 
     
 
-    //video switch
-    const btn = document.querySelector(".switch-btn");
-    const video = document.querySelector(".video-container");
+    // //video switch
+    // const btn = document.querySelector(".switch-btn");
+    // const video = document.querySelector(".video-container");
 
     
-    btn.addEventListener("click", function () {
-        if (!btn.classList.contains("slide")) {
-            btn.classList.add("slide");
-            video.pause();
-        } else {
-            btn.classList.remove("slide");
-            video.play();
-        }
-    });
+    // btn.addEventListener("click", function () {
+    //     if (!btn.classList.contains("slide")) {
+    //         btn.classList.add("slide");
+    //         video.pause();
+    //     } else {
+    //         btn.classList.remove("slide");
+    //         video.play();
+    //     }
+    // });
 
 
 
